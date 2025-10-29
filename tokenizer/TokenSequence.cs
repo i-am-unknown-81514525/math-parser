85cc1e1d2d13cc8c73a4db3dc356f81bdef964b5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-29 17:38:19 +0000

    Make parse function for token sequence

--- tokenizer/TokenSequence.cs
+++ tokenizer/TokenSequence.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace math_parser.tokenizer
 {
-    public class TokenSequence : IEnumerable<IToken>
+    public class TokenSequence : IEnumerable<IToken>, IToken
     {
         private List<IToken> tokens = new List<IToken>();
         private bool _writable = true;
@@ -57,6 +57,54 @@ namespace math_parser.tokenizer
         public static implicit operator TokenSequence(IToken[] tokens)
         {
             return new TokenSequence(tokens.ToArray());
+        }
+
+        public CharacterStream Parse(CharacterStream stream)
+        {
+            MakeImmutable();
+            foreach (IToken token in immutable_tokens)
+            {
+                token.Parse(stream);
+            }
+        }
+        public bool CanParse(CharacterStream stream)
+        {
+            MakeImmutable();
+            CharacterStream clone = stream.Fork();
+            foreach (IToken token in immutable_tokens)
+            {
+                token.Parse(clone);
+            }
+        }
+
+        public CharacterStream PartialParse(CharacterStream stream)
+        {
+            MakeImmutable();
+            CharacterStream ori = stream.Fork();
+            foreach (IToken token in immutable_tokens)
+            {
+                token.PartialParse(stream);
+                if (stream != ori)
+                {
+                    break;
+                }
+            }
+            return stream;
+        }
+        public bool CanPartialParse(CharacterStream stream)
+        {
+            MakeImmutable();
+            CharacterStream ori = stream.Fork();
+            CharacterStream curr = stream.Fork();
+            foreach (IToken token in immutable_tokens)
+            {
+                token.PartialParse(curr);
+                if (curr != ori)
+                {
+                    break;
+                }
+            }
+            return true;
         }
     }
 }
