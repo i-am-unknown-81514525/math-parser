4c49a10bad106fa05862da0383df1ca47fc671cc
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-31 01:40:16 +0000

    Add FracResult

---
+++ tokenizer/FracToken.cs
@@ -0,0 +1,15 @@
+namespace math_parser.tokenizer {
+    public struct FracResult : MathAtomResult
+    {
+        public readonly NumberResult top;
+        public readonly NumberResult bottom;
+
+        public FracValue(NumberResult top, NumberResult bottom)
+        {
+            this.top = top;
+            this.bottom = bottom;
+        }
+
+        public Fraction AsFraction() => top.AsFraction() / bottom.AsFraction();
+    }
+}
