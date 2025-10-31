98b59dbd9edb15f99c82ff548e4a11528b78d07a
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-31 22:43:26 +0000

    Change binding power so they won't be the exact same

--- tokenizer/Expression.cs
+++ tokenizer/Expression.cs
@@ -100,10 +100,10 @@ namespace math_parser.tokenizer
     {
         public static readonly Dictionary<string, (int, int)> binding_power = new Dictionary<string, (int, int)>()
         {
-            {"+", (1, 1) },
-            {"-", (1, 1) },
-            {"*", (2, 2) },
-            {"/", (2, 2) }
+            {"+", (1, 2) },
+            {"-", (1, 2) },
+            {"*", (3, 4) },
+            {"/", (3, 4) }
         };
 
         public Expression() : base(
