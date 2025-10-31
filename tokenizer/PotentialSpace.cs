e5ab07188b64068414d2edc1d90a4b6363d043b5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-31 16:10:06 +0000

    Also implement for math section

--- tokenizer/PotentialSpace.cs
+++ tokenizer/PotentialSpace.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace math_parser.tokenizer
 {
     public class PotentialSpace : Group<RepeatListResult<SyntaxDiscardResult>, SyntaxDiscardResult>
@@ -35,6 +37,24 @@ namespace math_parser.tokenizer
             CharacterStream cp = stream.Fork();
             inner_token.Parse(cp);
             return new SyntaxDiscardResult(stream.TakeTo(cp));
+        }
+    }
+
+    public class MathPotentialSpace : Group<SyntaxDiscardResult, MathLiteralResult>
+    {
+        public MathPotentialSpace() : base(new PotentialSpace()) { }
+        public override MathLiteralResult Parse(CharacterStream stream)
+        {
+            return new MathLiteralResult(inner_token.Parse(stream).content);
+        }
+    }
+
+    public class MathPotentialLine : Group<SyntaxDiscardResult, MathLiteralResult>
+    {
+        public MathPotentialLine() : base(new PotentialNewLine()) { }
+        public override MathLiteralResult Parse(CharacterStream stream)
+        {
+            return new MathLiteralResult(inner_token.Parse(stream).content);
         }
     }
 }
