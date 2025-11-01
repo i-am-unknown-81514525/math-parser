37d66224ed52b881778948848d8eb7c30b030cc5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-01 13:43:26 +0000

    Add ASTMul

--- ast/ASTOp.cs
+++ ast/ASTOp.cs
@@ -33,12 +33,33 @@ namespace math_parser.ast
         public ASTSub(IASTNode<I> left, IASTNode<I> right) : base(left, right) { }
 
         public override O Calc() => left.Calc().Sub(right.Calc());
-    } 
+    }
 
     public class ASTSub<T> : ASTOpNode2<T> where T : ISub<T, T>
     {
         public ASTSub(IASTNode<T> left, IASTNode<T> right) : base(left, right) { }
 
         public override T Calc() => left.Calc().Sub(right.Calc());
+    } 
+
+    public class ASTMul<L, R, O> : ASTOpNode2<L, R, O> where L : IMul<R, O>
+    {
+        public ASTMul(IASTNode<L> left, IASTNode<R> right) : base(left, right) { }
+
+        public override O Calc() => left.Calc().Mul(right.Calc());
+    }
+
+    public class ASTMul<I, O> : ASTOpNode2<I, O> where I : IMul<I, O>
+    {
+        public ASTMul(IASTNode<I> left, IASTNode<I> right) : base(left, right) { }
+
+        public override O Calc() => left.Calc().Mul(right.Calc());
+    } 
+
+    public class ASTMul<T> : ASTOpNode2<T> where T : IMul<T, T>
+    {
+        public ASTMul(IASTNode<T> left, IASTNode<T> right) : base(left, right) { }
+
+        public override T Calc() => left.Calc().Mul(right.Calc());
     } 
 }
