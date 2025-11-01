499a30d3d90350cf97c6ee6c386d884687dd4fd9
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-01 13:42:37 +0000

    Add ASTSub

--- ast/ASTOp.cs
+++ ast/ASTOp.cs
@@ -20,4 +20,25 @@ namespace math_parser.ast
 
         public override T Calc() => left.Calc().Add(right.Calc());
     } 
+
+    public class ASTSub<L, R, O> : ASTOpNode2<L, R, O> where L : ISub<R, O>
+    {
+        public ASTSub(IASTNode<L> left, IASTNode<R> right) : base(left, right) { }
+
+        public override O Calc() => left.Calc().Sub(right.Calc());
+    }
+
+    public class ASTSub<I, O> : ASTOpNode2<I, O> where I : ISub<I, O>
+    {
+        public ASTSub(IASTNode<I> left, IASTNode<I> right) : base(left, right) { }
+
+        public override O Calc() => left.Calc().Sub(right.Calc());
+    } 
+
+    public class ASTSub<T> : ASTOpNode2<T> where T : ISub<T, T>
+    {
+        public ASTSub(IASTNode<T> left, IASTNode<T> right) : base(left, right) { }
+
+        public override T Calc() => left.Calc().Sub(right.Calc());
+    } 
 }
